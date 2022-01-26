99467d0d961fb8e5c93b4692650325e13c30e97a
michael Means <colemeans51@gmail.com>
2022-01-26 16:27:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ const db = mysql.createConnection(
   console.log("connected to the election database.")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 
 //get a single candidate
 db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
@@ -27,6 +27,25 @@ db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
         console.log(err);
     } console.log( row);
 });
+
+//delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     } console.log(result);
+// });
+
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+                const params = [1, 'Ronald', 'Firbank', 1];
+
+                db.query(sql, params, (err, result) => {
+                    if (err) {
+                        console.log(err);
+                    } 
+                    console.log(result);
+                });
 
 //default response for any other request
 app.use((req, res) => {
