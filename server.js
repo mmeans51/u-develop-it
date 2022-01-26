dd150120ef077ba9fdffc0d52d79826b311f215e
michael Means <colemeans51@gmail.com>
2022-01-26 15:45:18 -0600

    adding server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+const express = require('express');
+const PORT = process.emitWarning.PORT || 3001;
+const app = express();
+//express middleware
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Pilgore1!',
+        database: 'election'
+    }, 
+    console.log('connected to the election database.')
+);
+
+//default response for any other request
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
