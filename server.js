a82086076cd6fc7529f769f5eed6d886b960795a
michael Means <colemeans51@gmail.com>
2022-01-31 13:04:55 -0600

    corrections

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
-const db = require("./db/connection");
-const apiRoutes = require("./routes/apiRoutes");
+const express = require('express');
+const db = require('./db/connection');
+const apiRoutes = require('./routes/apiRoutes');
 
-const PORT = process.emitWarning.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //express middleware
